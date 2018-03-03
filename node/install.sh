6c67e29d5f73dfebf7029dc8735adefb7ff252bf
Jonathan Borg <jonathan.borg@weahead.se>
2018-03-03 10:13:00 +0100

    added sketch to brewfile

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,8 @@
-if test ! $(which spoof)
-then
-  npm install spoof -g
-fi
+# if test ! $(which spoof)
+# then
+#   npm install spoof -g
+# fi
+nvm install 8.7
+nvm use 8.7 --default
+
+npm install pure-prompt -g
