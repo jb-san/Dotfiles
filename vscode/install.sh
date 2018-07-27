ac69298e8826694fb095b84545afb39ff6ad3858
Jonathan <jonathan@x3m.io>
2018-07-28 00:45:13 +0200

    removed vscode stuff, now use the extension settings sync

--- vscode/install.sh
+++ vscode/install.sh
@@ -10,9 +10,3 @@ while read -r name
 do
     code --install-extension "${name}"
 done < "extensions.txt"
-
-# Symlink settings, keybindings and snippets
-# TODO: make this part dynamic
-ln -s /Users/jb/.dotfiles/vscode/settings.json /Users/jb/Library/Application\ Support/Code/User/settings.json
-ln -s /Users/jb/.dotfiles/vscode/keybindings.json /Users/jb/Library/Application\ Support/Code/User/keybindings.json
-ln -s /Users/jb/.dotfiles/vscode/snippets/ /Users/jb/Library/Application\ Support/Code/User/snippets
