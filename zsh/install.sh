9b5a54356e18ef46d87e403a102b89f4d60c1c5b
Jonathan <jonathan@x3m.io>
2018-07-28 16:17:03 +0200

    fixed path

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,7 +2,6 @@
 #
 # Antigen, the zsh package manager
 #
-echo 'Setting Zsh to default shell'
-sudo echo '/usr/local/bin/zsh' >> etc/shells
+sudo echo '/usr/local/bin/zsh' >> /etc/shells
 chsh -s /usr/local/bin/zsh
 curl -L git.io/antigen > antigen.zsh
