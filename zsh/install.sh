a49b682e43b4b5e8d00181ba95048532a7cc175c
Jonathan <jonathan@x3m.io>
2018-07-28 16:21:14 +0200

    shell stuff

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,6 +2,6 @@
 #
 # Antigen, the zsh package manager
 #
-sudo echo '/usr/local/bin/zsh' >> /etc/shells
+echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
 curl -L git.io/antigen > antigen.zsh
