0b31c7320df09bdcc8f778489d173c3ffcff27d2
Jonathan <jonathan@x3m.io>
2018-07-28 15:36:02 +0200

    testing with nvm just from antigen

--- node/install.sh
+++ node/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export NVM_DIR="$HOME/.nvm"
-source "/usr/local/opt/nvm/nvm.sh"
-nvm install 8.7
-nvm use 8.7 --default
+# export NVM_DIR="$HOME/.nvm"
+# source "/usr/local/opt/nvm/nvm.sh"
+# nvm install 8.7
+# nvm use 8.7 --default
