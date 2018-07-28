fd423e71cea3d1aeac9864b185faaac12fd72263
Jonathan <jonathan@x3m.io>
2018-07-28 16:49:53 +0200

    moved around

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,4 +4,6 @@
 #
 echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
-curl -L git.io/antigen > antigen.zsh
+
+# get newest antigen version
+# curl -L git.io/antigen > antigen.zsh
