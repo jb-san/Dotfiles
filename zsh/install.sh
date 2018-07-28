56355638fe29ecccc717b06dc65b31bdb0788db8
Jonathan <jonathan@x3m.io>
2018-07-28 15:48:05 +0200

    fixing default shell error

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,5 +2,7 @@
 #
 # Antigen, the zsh package manager
 #
+echo 'Setting Zsh to default shell'
+sudo echo '/usr/local/bin/zsh' >> etc/shells
 chsh -s /usr/local/bin/zsh
 curl -L git.io/antigen > antigen.zsh
