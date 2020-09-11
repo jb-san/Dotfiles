01efa8a34fa09974d2bc6fe908aca23a65c8e96d
Jonathan Borg <jonathan@x3m.io>
2020-09-11 22:03:38 +0200

    chore: remove old apps

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,4 +6,4 @@ echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
 
 # get newest antigen version
-# curl -L git.io/antigen > antigen.zsh
+curl -L git.io/antigen > antigen.zsh
