4a4832424ed141b2e2902c22710de61baf8fe290
Jonathan Borg <jonathan@aukilabs.com>
2026-02-25 10:22:26 +0800

    some updates

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$HOME/.cargo/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export ANDROID_HOME=$HOME/Library/Android/sdk
